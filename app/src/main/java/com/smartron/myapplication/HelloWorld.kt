e61a12991da87548fec6683d10faf91930f340dc
sneha <sneha.gangireddy@smartron.com>
2018-10-03 16:09:43 +0530

    first commit kotlin intent example

---
+++ app/src/main/java/com/smartron/myapplication/HelloWorld.kt
@@ -0,0 +1,26 @@
+package com.smartron.myapplication
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_helloworld.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.AccessControlContext
+
+class HelloWorld : AppCompatActivity() {
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, HelloWorld::class.java))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helloworld)
+
+        textMsg.setText(intent.extras.getString("text"))
+
+    }
+}
---
+++ app/src/main/java/com/smartron/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.smartron.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.smartron.myapplication.R.id.message
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nextButton.setOnClickListener {
+
+            /* val (parameter type "val" or "var") -> it will decide whether that textView is static or dynamic
+            *  message(variable name)
+             * String (Data type : it may be String, int, float, double)
+             * text (represents getText() or setText()*/
+
+            val message: String = helloWorld.text.toString()
+
+            /*Explicit Intent, first parameter says the current activity and second parameter tells the destination activity
+            * destination activity defined like DestinationActivity::class.java*/
+
+            val intent = Intent(this@MainActivity, HelloWorld::class.java)
+
+            // putExtra to pass the message to another activity using key, value parameters
+            intent.putExtra("text", message)
+
+            startActivity(intent)
+        }
+    }
+}
